1a1feafda6da741682020836f6c147ac59bad6cc
MasuqaT <occar@hotmail.co.jp>
2026-06-07 23:05:52 +0900

    Implement helper tester

--- packages/mdast-util-gherkin/src/util.ts
+++ packages/mdast-util-gherkin/src/util.ts
@@ -1,7 +1,8 @@
-import type { Heading, ListItem } from "mdast";
+import type { Heading, ListItem, Node } from "mdast";
 import { GherkinTypes } from "./constant.ts";
 import { findAllAfter } from "unist-util-find-all-after";
 import { toString } from "mdast-util-to-string";
+import type { GherkinNodes } from "./types.ts";
 
 /**
  * Get the name of a Gherkin segment from a heading node.
@@ -50,4 +51,37 @@ export function getStepName(node: ListItem): string | undefined {
 
   const nameNodes = findAllAfter(paragraph, separator);
   return toString(nameNodes).trim();
+}
+
+type GherkinKeywords = (typeof GherkinTypes)[keyof typeof GherkinTypes];
+
+type GherkinNodeMap = {
+  [K in GherkinKeywords]: Extract<GherkinNodes, { data: { gherkin: { type: K } } }>;
+};
+
+/**
+ * Test if a node is a Gherkin node.
+ * @returns
+ *   Function that tests if a node is a Gherkin node.
+ */
+export function testGherkinNode(): (node: Node) => node is GherkinNodes;
+/**
+ * Test if a node is a Gherkin node of a specific type.
+ * @param type
+ *   Type of Gherkin node to test for.
+ * @returns
+ *   Function that tests if a node is a Gherkin node of the specified type.
+ */
+export function testGherkinNode<T extends GherkinKeywords>(
+  type: T,
+): (node: Node) => node is GherkinNodeMap[T];
+
+export function testGherkinNode<T extends GherkinKeywords>(type?: T) {
+  if (!type) {
+    return (node: Node) => Object.values(GherkinTypes).some((t) => node.data?.gherkin?.type === t);
+  }
+
+  return (node: Node) => {
+    return node.data?.gherkin?.type === type;
+  };
 }
